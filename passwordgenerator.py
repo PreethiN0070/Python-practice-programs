fba4ed7c1bf3b70d620da56ae71a6ec760ba64e8
Preethi N <79077181+PreethiN0070@users.noreply.github.com>
2023-12-12 16:01:47 +0530

    Create passwordgenerator.py

---
+++ passwordgenerator.py
@@ -0,0 +1,6 @@
+##password generator
+import random
+passlen = int(input("enter the length of password"))
+s="abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?"
+p = "".join(random.sample(s,passlen ))
+print(p)
